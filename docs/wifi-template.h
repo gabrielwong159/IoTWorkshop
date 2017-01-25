ac8af6b1f972d04d4fcf691f5daba85280c9433b
gabrielwong159 <gabrielwong159@gmail.com>
2017-01-25 12:16:19 +0800

    Initial commit

---
+++ docs/wifi-template.h
@@ -0,0 +1,7 @@
+#ifndef wifi_h
+#define wifi_h
+
+const char* _ssid = "/* insert ssid here */";
+const char* _password = "/* insert password here */";
+
+#endif
---
+++ wifi-server.ino
@@ -0,0 +1,131 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <Adafruit_NeoPixel.h>
+
+#include "index.h"
+#include "wifi.h"
+
+const char* ssid = _ssid;
+const char* password = _password;
+
+ESP8266WebServer server(80);
+MDNSResponder mdns;
+
+const int STRIP_LENGTH = 60;
+const int PIN_NUMBER = 1; //D10
+const int LED_BRIGHTNESS = 31;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIP_LENGTH, PIN_NUMBER);
+
+String page = file1;
+
+uint32_t red = strip.Color(255,0,0);
+uint32_t orange = strip.Color(255,127,0);
+uint32_t yellow = strip.Color(255,255,0);
+uint32_t green = strip.Color(0,255,0);
+uint32_t blue = strip.Color(0,0,255);
+uint32_t indigo = strip.Color(75,0,130);
+uint32_t violet = strip.Color(148,0,211);
+uint32_t black = strip.Color(0,0,0);
+
+void setup() {
+  setupWifi();
+  setupServer();
+  setupLed();
+}
+
+void loop() {
+  server.handleClient();
+}
+
+/* ==================================================================== */
+
+void custom1() {  
+  uint32_t colors[7] = {red, orange, yellow, green, blue, indigo, violet};
+
+  for (int i=0; i<STRIP_LENGTH; i++) strip.setPixelColor(i, colors[i%7]);
+  strip.show();
+}
+
+void custom2() {
+  //alternating colours
+  uint32_t pink = strip.Color(255,192,203);
+  uint32_t purple = strip.Color(128,0,128);
+  
+  for (int i=0; i<STRIP_LENGTH; i++) strip.setPixelColor(i, i%2 ? pink : purple);
+  strip.show();
+}
+
+/* ==================================================================== */
+
+void setupWifi() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  WiFi.begin(ssid, password); 
+  delay(1000);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+
+    //a flashing LED means the ESP is not connected
+    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+  }
+  //a stable LED on means the ESP is now connected
+  digitalWrite(LED_BUILTIN,true);
+
+  mdns.begin("esp8266", WiFi.localIP());
+}
+
+void setupServer() {
+  server.on("/", [](){
+    server.send(200, "text/html", page);
+  });
+  
+  server.on("/red", [](){
+    server.send(200, "text/html", page);
+    showColor(red);
+  });
+
+  server.on("/green", [](){
+    server.send(200, "text/html", page);
+    showColor(green);
+  });
+
+  server.on("/blue", [](){
+    server.send(200, "text/html", page);
+    showColor(blue);
+  });
+
+  server.on("/off", [](){
+    server.send(200, "text/html", page);
+    showColor(black);
+  });
+
+  server.on("/custom1", [](){
+    server.send(200, "text/html", page);
+    custom1();
+  });
+
+  server.on("/custom2", [](){
+    server.send(200, "text/html", page);
+    custom2();
+  });
+  
+  server.begin();
+}
+
+void setupLed() {
+  strip.begin();
+  strip.show();
+  strip.setBrightness(LED_BRIGHTNESS);
+}
+
+void showColor(uint32_t color) {
+  for (int i=0; i<STRIP_LENGTH; i++) strip.setPixelColor(i,color);
+  strip.show();
+}
+
+void showColor(uint8_t r, uint8_t g, uint8_t b) {
+  for (int i=0; i<STRIP_LENGTH; i++) strip.setPixelColor(i,r,g,b);
+  strip.show();
+}
